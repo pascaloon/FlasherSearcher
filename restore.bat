d343d9d147d36fc7fb61c1f477d9d781fcb2f789
Pascal <pascalasselin@hotmail.com>
2020-10-15 21:10:42 -0400

    Added estore.bat utility

---
+++ restore.bat
@@ -0,0 +1,3 @@
+set conan="C:\Users\Pascal\AppData\Roaming\Python\Python39\Scripts\conan.exe"
+%conan% install .\conanfile.txt -s build_type=Debug
+%conan% install .\conanfile.txt -s build_type=Release
